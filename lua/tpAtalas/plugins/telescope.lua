1bc5932238fe0bdad4a3907ffa131bc37d199f0b
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2022-11-19 16:15:34 -0600

    refactor: Refactored the telescope configuration (#10)

--- lua/tpAtalas/plugins/telescope.lua
+++ lua/tpAtalas/plugins/telescope.lua
@@ -1,27 +1,40 @@
 -- import telescope plugin safely
 local telescope_setup, telescope = pcall(require, "telescope")
 if not telescope_setup then
-  return
+	return
 end
 
 -- import telescope actions safely
 local actions_setup, actions = pcall(require, "telescope.actions")
 if not actions_setup then
-  return
+	return
 end
 
 -- configure telescope
 telescope.setup({
-  -- configure custom mappings
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-        ["<C-j>"] = actions.move_selection_next, -- move to next result
-        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+	-- configure custom mappings
+	defaults = {
+		layout_config = {
+      horizontal = {
+        width = 0.95,
+			  -- preview_width = 0.55,
       },
-    },
+		},
+		mappings = {
+			i = {
+				["<C-k>"] = actions.move_selection_previous, -- move to prev result
+				["<C-j>"] = actions.move_selection_next, -- move to next result
+				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+			},
+		},
+	},
+  pickers = {
+    find_files = {
+      -- theme = "dropdown",
+
+    }
   },
+
 })
 
 telescope.load_extension("fzf")
