48466e9e923d7fb895e69075d275fd1c1eead832
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2022-12-11 15:12:12 -0600

    refactor: Refactored the kemap in lspconfig

--- lua/tpAtalas/plugins/lsp/lspconfig.lua
+++ lua/tpAtalas/plugins/lsp/lspconfig.lua
@@ -29,8 +29,8 @@ local on_attach = function(client, bufnr)
 	keymap.set('n', '<leader>rn', '<cmd>Lspsaga rename<CR>', opts) -- smart rename
 	keymap.set('n', '<leader>d', '<cmd>Lspsaga show_line_diagnostics<CR>', opts) -- show  diagnostics for line
 	keymap.set('n', '<leader>d', '<cmd>Lspsaga show_cursor_diagnostics<CR>', opts) -- show diagnostics for cursor
-	keymap.set('n', '[d', '<cmd>Lspsaga diagnostic_jump_prev<CR>zz', opts) -- jump to previous diagnostic in buffer
-	keymap.set('n', ']d', '<cmd>Lspsaga diagnostic_jump_next<CR>zz', opts) -- jump to next diagnostic in buffer
+	keymap.set('n', '[d', '<cmd>Lspsaga diagnostic_jump_prev<CR>', opts) -- jump to previous diagnostic in buffer
+	keymap.set('n', ']d', '<cmd>Lspsaga diagnostic_jump_next<CR>', opts) -- jump to next diagnostic in buffer
 	keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<CR>', opts) -- show documentation for what is under cursor
 	keymap.set('n', '<leader>o', '<cmd>LSoutlineToggle<CR>', opts) -- see outline on right hand side
 	keymap.set('n', '<space>wa', '<cmd>add_workspace_folder<CR>', opts)
