a62e35d04ec33ff6639ffb4167f367add2f91f8a
tpAtalas <tp@atalas.dev>
2022-12-17 12:15:33 -0600

    feat: Added the custom highlight color for `Search` (#97)

--- lua/user/core/colorscheme.lua
+++ lua/user/core/colorscheme.lua
@@ -3,6 +3,7 @@ local groups = {
 	all = {
 		Whitespace = { link = 'Comment' },
 		IncSearch = { fg = '', bg = 'palette.bg4' },
+		Search = { fg = 'palette.yellow.dim', bg = '' },
 		ColorColumn = { bg = 'palette.bg3' },
 		CursorLineNr = { fg = 'palette.yellow.dim', bg = 'palette.bg3' },
 		CursorLine = { bg = 'palette.bg3' },
@@ -15,15 +16,14 @@ local groups = {
 -- Default options
 require('nightfox').setup({
 	options = {
-		-- Compiled file's destination location
 		compile_path = vim.fn.stdpath('cache') .. '/nightfox',
-		compile_file_suffix = '_compiled', -- Compiled file suffix
-		transparent = true, -- Disable setting background
-		terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-		dim_inactive = false, -- Non focused panes set to alternative background
-		module_default = true, -- Default enable value for modules
-		styles = { -- Style to be applied to different syntax groups
-			comments = 'italic', -- Value is any valid attr-list value `:help attr-list`
+		compile_file_suffix = '_compiled',
+		transparent = true,
+		terminal_colors = true,
+		dim_inactive = false,
+		module_default = true,
+		styles = {
+			comments = 'italic',
 			conditionals = 'NONE',
 			constants = 'NONE',
 			functions = 'italic',
@@ -34,12 +34,12 @@ require('nightfox').setup({
 			types = 'NONE',
 			variables = 'NONE',
 		},
-		inverse = { -- Inverse highlight for different types
+		inverse = {
 			match_paren = false,
 			visual = false,
 			search = false,
 		},
-		modules = { -- List of various plugins and additional options
+		modules = {
 		},
 	},
 	palettes = {},
@@ -47,5 +47,5 @@ require('nightfox').setup({
 	groups = groups,
 })
 
--- setup must be called before loading
+
 vim.cmd('colorscheme terafox')
