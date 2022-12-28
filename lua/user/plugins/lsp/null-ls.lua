6bc3166771c7f597d91038179df8684cb73479f7
tpAtalas <tp@atalas.dev>
2022-12-28 02:34:38 -0600

    feat: Updated the settings and added the `cspell` to `null-ls` (#118)
    Updated the settings for `lspconfig` to lively update the diagnostics.
    This may cause performance issues, and the `virtual_text` is disabled to
    reduce performance bottleneck.
    
    Added the `cspell` diagnostics and codeactions to `null-ls`.
    Additionally, the custom file directory function for the `cspell.json` is added
    to integrate the dictionaries across multiple workspaces. This may increase i
    the usability; however, the performance may cause issues later.

--- lua/user/plugins/lsp/null-ls.lua
+++ lua/user/plugins/lsp/null-ls.lua
@@ -1,23 +1,66 @@
--- import null-ls plugin safely
 local setup, null_ls = pcall(require, 'null-ls')
 if not setup then
 	return
 end
 
 -- for conciseness
-local formatting = null_ls.builtins.formatting -- to setup formatters
-local diagnostics = null_ls.builtins.diagnostics -- to setup linters
-local codeactions = null_ls.builtins.code_actions -- to setup code action
-local completion = null_ls.builtins.completion -- to setup completion
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+local codeactions = null_ls.builtins.code_actions
+local completion = null_ls.builtins.completion
 
-local eslint = { -- js/ts linter
-	-- only enable eslint if root has .eslintrc.js
+local eslint = {
 	condition = function(utils)
-		return utils.root_has_file('.eslintrc.json') -- change file extension if you use something else
+		return utils.root_has_file('.eslintrc.json')
 	end,
 }
 
--- to setup format on save
+--------------------
+-- cspell setting --
+--------------------
+local cspell_custom_file_path = vim.fn.findfile('~/.config/cspell/cspell.json')
+
+local cspell_config_file_path = function()
+	local custom_path = nil
+	if cspell_custom_file_path ~= '' then
+		custom_path = cspell_custom_file_path
+	end
+	return custom_path
+end
+
+local cspell_extra_args = function()
+	if cspell_config_file_path ~= nil then
+		return {
+			'--config',
+			cspell_custom_file_path,
+		}
+	else
+		return nil
+	end
+end
+
+local cspell = {
+	config = {
+		create_config_file = true,
+		find_json = cspell_config_file_path,
+	},
+	filetypes = {
+		'markdown',
+		'text',
+		'lua',
+		'javascript',
+		'javascriptreact',
+		'typescript',
+		'typescriptreact',
+		'json',
+		'css',
+	},
+	disable_filetypes = {
+		'NvimTree',
+	},
+	extra_args = cspell_extra_args,
+}
+
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
 -- Built-in sources:
@@ -26,10 +69,10 @@ local source = {
 	diagnostics.tsc,
 	-- WARNING: diagnostics listed below do not support workspace level diagnostics
 	formatting.markdownlint,
-	diagnostics.codespell,
-	formatting.codespell,
-	formatting.stylua, -- lua formatter
-	formatting.prettier, -- js/ts formatter
+	formatting.stylua,
+	formatting.prettier,
+	codeactions.cspell.with(cspell),
+	diagnostics.cspell.with(cspell),
 	codeactions.eslint_d.with(eslint),
 	diagnostics.eslint_d.with(eslint),
 }
@@ -43,7 +86,6 @@ local on_attach = function(current_client, bufnr)
 			callback = function()
 				vim.lsp.buf.format({
 					filter = function(client)
-						--  only use null-ls for formatting instead of lsp server
 						return client.name == 'null-ls'
 					end,
 					bufnr = bufnr,
@@ -53,9 +95,7 @@ local on_attach = function(current_client, bufnr)
 	end
 end
 
--- configure null_ls
 null_ls.setup({
-	-- setup formatters & linters
 	sources = source,
 	on_attach = on_attach,
 	debug = false,
@@ -64,6 +104,6 @@ null_ls.setup({
 	virtual_text = true,
 	signs = true,
 	severity_sort = true,
-	update_in_insert = false,
-	debounce = 1000,
+	update_in_insert = true,
+	debounce = 350,
 })
