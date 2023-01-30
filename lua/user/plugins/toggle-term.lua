67df4a2e094dd266d7cfebca7937cded34b4fe2b
tpAtalas <tp@atalas.dev>
2023-01-30 14:58:41 -0600

    feat: Added the keymap to toggle-term and telescope (#175)
    Now `lazy-git` within toggle-term and `telescope` close on pressing
    `escape` key.

--- lua/user/plugins/toggle-term.lua
+++ lua/user/plugins/toggle-term.lua
@@ -33,7 +33,7 @@ return {
 
 		function _G.set_terminal_keymaps()
 			local opts = { noremap = true }
-			-- vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+			-- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
 			vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
 			vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
 			vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
@@ -55,7 +55,7 @@ return {
 			-- function to run on opening the terminal
 			on_open = function(term)
 				vim.cmd('startinsert!')
-				vim.api.nvim_buf_set_keymap(term.bufnr, 'n', '<q>', '<cmd>close<CR>', { noremap = true, silent = true })
+				vim.api.nvim_buf_set_keymap(term.bufnr, 't', '<esc>', 'q<CR>', { noremap = true, silent = true })
 			end,
 			-- function to run on closing the terminal
 			on_close = function(term)
