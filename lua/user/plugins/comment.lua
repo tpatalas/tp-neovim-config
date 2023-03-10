7dfa0eb7eb684e15c5549f00af637a9ea70333dd
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-03-10 17:04:28 -0600

    refactor: refactor the keymaps for spelling and commenting
    Update the keymaps for the spelling checks and update, and now
    commenting keymap is `?`, which include the line and block comment.

--- lua/user/plugins/comment.lua
+++ lua/user/plugins/comment.lua
@@ -7,11 +7,11 @@ return {
 		require('Comment').setup({
 			pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
 			toggler = {
-				line = '<leader>co',
+				line = '?',
 				block = '"',
 			},
 			opleader = {
-				line = '<leader>co',
+				line = '?',
 				BLock = '"',
 			},
 		})
