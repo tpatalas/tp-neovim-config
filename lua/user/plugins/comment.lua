9c03cd732aca1689c8120692f9d9b754e257d71c
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-03-17 14:25:50 -0500

    feat: update the comment plugins' keymap
    Revert the keymaps of comment plugins to '<leader>co'.

--- lua/user/plugins/comment.lua
+++ lua/user/plugins/comment.lua
@@ -7,11 +7,11 @@ return {
 		require('Comment').setup({
 			pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
 			toggler = {
-				line = 'co',
+				line = '<leader>co',
 				block = '"',
 			},
 			opleader = {
-				line = 'co',
+				line = '<leader>co',
 				BLock = '"',
 			},
 		})
