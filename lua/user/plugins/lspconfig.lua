56d62e3d7b8d2f1564c025eb2ce159cab821a6dc
tpAtalas <64714719+tpAtalas@users.noreply.github.com>
2023-04-04 15:50:26 -0500

    feat: update the goto definition and goto type with lspsaga

--- lua/user/plugins/lspconfig.lua
+++ lua/user/plugins/lspconfig.lua
@@ -27,7 +27,8 @@ return {
 			keymap.set('n', 'gf', '<cmd>Lspsaga lsp_finder<CR>', opts) -- show definition, references
 			keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts) -- got to declaration
 			keymap.set('n', 'gd', '<cmd>Lspsaga peek_definition<CR>', opts) -- see definition and make edits in window
-			keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts) -- go to implementation
+			keymap.set('n', 'gi', '<cmd>Lspsaga goto_definition<CR>', opts) -- go to implementation
+			keymap.set('n', 'gt', '<cmd>Lspsaga goto_type_definition<CR>', opts) -- go to Type definition
 			keymap.set('n', '<leader>ca', '<cmd>Lspsaga code_action<CR>', opts) -- see available code actions
 			keymap.set('n', '<leader>rn', '<cmd>Lspsaga rename<CR>', opts) -- smart rename
 			keymap.set('n', '<leader>d', '<cmd>Lspsaga show_line_diagnostics<CR>')
