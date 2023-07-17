436346169e7600827f7710daed03ba3e44675d79
tpatalas <tp@atalas.dev>
2023-07-17 07:53:32 -0500

    refactor: Update the Neo-tree plugin
    Update the version to 3.x from 2.x of the Neo-tree as new version has
    been introduced.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,10 +1,8 @@
--- more info:
--- https://github.com/nvim-neo-tree/neo-tree.nvim
 local noremap = { noremap = true, silent = true }
 
 return {
 	'nvim-neo-tree/neo-tree.nvim',
-	branch = 'v2.x',
+	branch = 'v3.x',
 	dependencies = {
 		'MunifTanjim/nui.nvim',
 	},
