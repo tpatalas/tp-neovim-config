0da2fc2ffe1b40d03dabb7939492ee4ceb88f881
tpatalas <tp@atalas.dev>
2023-07-23 10:13:11 -0500

    refactor: Lualine file structure reorganization
    Revised the Lualine file structure for improved organization.

---
+++ lua/plugins/lualine/custom_theme.lua
@@ -0,0 +1,45 @@
+return function(palette)
+	return {
+
+		normal = {
+			a = { fg = palette.blue.base, bg = palette.bg0 },
+			b = { fg = palette.fg3, bg = palette.bg0 },
+			c = { fg = palette.fg3, bg = palette.bg0 },
+			x = { fg = palette.fg2, bg = palette.bg0 },
+			y = { fg = palette.fg3, bg = palette.bg0 },
+			z = { fg = palette.blue.base, bg = palette.bg0 },
+		},
+		insert = {
+			a = { fg = palette.yellow.base, bg = palette.bg0 },
+			b = { fg = palette.yellow.base, bg = palette.bg0 },
+			c = { fg = palette.yellow.base, bg = palette.bg0 },
+			x = { fg = palette.yellow.base, bg = palette.bg0 },
+			y = { fg = palette.yellow.base, bg = palette.bg0 },
+			z = { fg = palette.yellow.base, bg = palette.bg0 },
+		},
+		visual = {
+			a = { fg = palette.magenta.base, bg = palette.bg0 },
+			b = { fg = palette.magenta.base, bg = palette.bg0 },
+			c = { fg = palette.magenta.base, bg = palette.bg0 },
+			x = { fg = palette.magenta.base, bg = palette.bg0 },
+			y = { fg = palette.magenta.base, bg = palette.bg0 },
+			z = { fg = palette.magenta.base, bg = palette.bg0 },
+		},
+		command = {
+			a = { fg = palette.orange.base, bg = palette.bg0 },
+			b = { fg = palette.orange.base, bg = palette.bg0 },
+			c = { fg = palette.orange.base, bg = palette.bg0 },
+			x = { fg = palette.orange.base, bg = palette.bg0 },
+			y = { fg = palette.orange.base, bg = palette.bg0 },
+			z = { fg = palette.orange.base, bg = palette.bg0 },
+		},
+		inactive = {
+			a = { fg = palette.fg0, bg = palette.bg0 },
+			b = { fg = palette.fg0, bg = palette.bg0 },
+			c = { fg = palette.fg0, bg = palette.bg0 },
+			x = { fg = palette.fg0, bg = palette.bg0 },
+			y = { fg = palette.fg0, bg = palette.bg0 },
+			z = { fg = palette.fg0, bg = palette.bg0 },
+		},
+	}
+end
