dc8e4ddea9744eb3413b11041d31d6cc1d6f703f
tpatalas <tp@atalas.dev>
2023-08-28 09:06:54 -0500

    feat: Update the auto-session configs

--- lua/plugins/auto_session/init.lua
+++ lua/plugins/auto_session/init.lua
@@ -8,8 +8,8 @@ return {
 		require('auto-session').setup({
 			bypass_session_save_file_types = { '', 'blank', 'neo-tree', 'noice', 'notify', 'oil' },
 			log_level = 'error',
-			auto_session_enable_last_session = true,
-			-- auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+			auto_session_enable_last_session = false,
+			auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
 		})
 	end,
 }
