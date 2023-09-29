0a1bf9fb01fe4dcd3925886399146826e855e94b
tpatalas <tp@atalas.dev>
2023-09-29 15:45:44 -0500

    refactor: Refactor the indent_blankline plugin
    Move indent_blankline from plugin init file into dedicated plugin file.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,18 +12,4 @@ return {
 			vim.g.matchup_matchparen_offscreen = ''
 		end,
 	},
-	{
-		'lukas-reineke/indent-blankline.nvim',
-		event = 'BufRead',
-		config = function()
-			require('indent_blankline').setup({
-				space_char_blankline = ' ',
-				show_current_context = true,
-				show_current_context_start = false,
-				char_highlight_list = {
-					'IndentBlankLineIndent',
-				},
-			})
-		end,
-	},
 }
