1a6799afdbd0fdf3279b68964124c0de4e1703dd
tpatalas <tp@atalas.dev>
2023-10-14 08:26:30 -0500

    fix: Fix the markdown-preview updating issue

--- lua/plugins/markdown_preview/init.lua
+++ lua/plugins/markdown_preview/init.lua
@@ -2,12 +2,12 @@ local noremap = { noremap = true, silent = true }
 
 return {
 	'iamcco/markdown-preview.nvim',
-	build = 'cd app && npm install',
 	lazy = true,
-	keys = {
-		{ '<leader>mpo', ':MarkdownPreview<CR>', noremap }, -- start markdown preview open
-		{ '<leader>mps', ':MarkdownPreviewStop<CR>', noremap }, -- stop markdown preview stop
-	},
+	cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+	ft = { 'markdown' },
+	build = function()
+		vim.fn['mkdp#util#install']()
+	end,
 	config = function()
 		local browser_path = '/Applications/Firefox.app'
 		-- comment browser_path if you want to use the default browser
@@ -23,5 +23,4 @@ return {
 		vim.g.mkdp_filetypes = { 'markdown' }
 		vim.g.mkdp_browser = custom_browser_path()
 	end,
-	ft = { 'markdown' },
 }
