fda6b4b312c7656c7921d0e24e124cfa7d6e7fb9
tpatalas <tp@atalas.dev>
2023-11-03 11:42:48 -0500

    feat: Update the configs of none-ls
    Now prettier formatting only works when there is prettier config file in the root.

--- lua/plugins/none-ls/configs.lua
+++ lua/plugins/none-ls/configs.lua
@@ -10,6 +10,32 @@ M.eslint = {
 	end,
 }
 
+--------------
+-- prettier --
+--------------
+M.prettier = {
+	condition = function(utils)
+		local config_files = {
+			'prettier.config.js',
+			'prettier.config.json',
+			'.prettierrc',
+			'.prettierrc.js',
+			'.prettierrc.json',
+			'.prettierrc.toml',
+			'.prettierrc.yml',
+			'.prettierrc.yaml',
+		}
+
+		for _, filename in ipairs(config_files) do
+			if utils.root_has_file(filename) then
+				return true
+			end
+		end
+
+		return false
+	end,
+}
+
 ------------
 -- cspell --
 ------------
