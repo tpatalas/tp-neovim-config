002c494e8dc6c086dd1ec2ad9f6f1741a67ecab8
tpatalas <tp@atalas.dev>
2023-11-03 13:29:37 -0500

    feat: Update the toggleTerm keymaps

--- lua/plugins/toggle_term/init.lua
+++ lua/plugins/toggle_term/init.lua
@@ -20,7 +20,7 @@ return {
 			start_in_insert = true,
 			insert_mappings = true,
 			persist_size = true,
-			direction = 'float', -- 'vertical' | 'horizontal' | 'tab' | 'float',
+			direction = 'tab', -- 'vertical' | 'horizontal' | 'tab' | 'float',
 			close_on_exit = true,
 			shell = vim.o.shell,
 			highlights = {
@@ -44,13 +44,14 @@ return {
 		})
 
 		function _G.set_terminal_keymaps()
-			local opts = { noremap = true }
-			-- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-			vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
-			vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-			vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-			vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-			vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+			local opts = { buffer = 0 }
+			vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+			vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+			vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+			vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+			vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+			vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+			vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 		end
 
 		vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
