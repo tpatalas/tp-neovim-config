64cc8edc59b73873fed61985a3602378a1c1a4bb
tpatalas <tp@atalas.dev>
2023-11-06 10:02:38 -0600

    feat: Install the oil.nvim

---
+++ lua/plugins/oil/init.lua
@@ -0,0 +1,45 @@
+local noremap = { noremap = true, silent = true }
+
+-- open the preview when oil is open
+-- vim.api.nvim_create_autocmd('User', {
+-- 	pattern = 'OilEnter',
+-- 	callback = vim.schedule_wrap(function(args)
+-- 		local oil = require('oil')
+-- 		if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+-- 			oil.select({ preview = true })
+-- 		end
+-- 	end),
+-- })
+
+return {
+	'stevearc/oil.nvim',
+	lazy = true,
+	opts = {},
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	keys = {
+		{ '<leader>ef', ':lua require(\'oil\').open(vim.fn.expand(\'%:p:h\'))<CR>', noremap }, -- open the current directory
+		{ '<leader>eo', ':Oil . <CR>', noremap }, -- open the root
+		{ '<leader>ec', ':lua require(\'oil\').discard_all_changes()<CR>', noremap }, -- clear all changes
+	},
+	config = function()
+		require('oil').setup({
+			columns = {
+				-- { 'mtime', highlight = 'OilMTime' },
+				{ 'icon', add_padding = true },
+				{ 'size', highlight = 'OilSize' },
+				-- "permissions",
+			},
+			keymaps = {
+				['g?'] = 'actions.show_help',
+				['<CR>'] = 'actions.select',
+				['Y'] = 'actions.select_vsplit',
+				['X'] = 'actions.select_split',
+				['P'] = 'actions.preview',
+				['q'] = 'actions.close',
+				['H'] = 'actions.toggle_hidden',
+				['<Home>'] = 'actions.parent',
+				['<End>'] = 'actions.open_cwd',
+			},
+		})
+	end,
+}
