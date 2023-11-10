aec1c3223ac9d6e20b97f1775a1ebe3ab0c123f3
tpatalas <tp@atalas.dev>
2023-11-09 18:08:38 -0600

    feat: Add the material colorscheme

---
+++ lua/plugins/lualine/custom_theme/material.lua
@@ -0,0 +1,44 @@
+return function(colors)
+	return {
+		normal = {
+			a = { fg = colors.blue, bg = colors.bg },
+			b = { fg = colors.fg, bg = colors.bg },
+			c = { fg = colors.fg, bg = colors.bg },
+			x = { fg = colors.fg, bg = colors.bg },
+			y = { fg = colors.fg, bg = colors.bg },
+			z = { fg = colors.green, bg = colors.bg },
+		},
+		insert = {
+			a = { fg = colors.orange, bg = colors.bg },
+			b = { fg = colors.orange, bg = colors.bg },
+			c = { fg = colors.orange, bg = colors.bg },
+			x = { fg = colors.orange, bg = colors.bg },
+			y = { fg = colors.orange, bg = colors.bg },
+			z = { fg = colors.orange, bg = colors.bg },
+		},
+		visual = {
+			a = { fg = colors.purple, bg = colors.bg },
+			b = { fg = colors.purple, bg = colors.bg },
+			c = { fg = colors.purple, bg = colors.bg },
+			x = { fg = colors.purple, bg = colors.bg },
+			y = { fg = colors.purple, bg = colors.bg },
+			z = { fg = colors.purple, bg = colors.bg },
+		},
+		command = {
+			a = { fg = colors.yellow, bg = colors.bg },
+			b = { fg = colors.yellow, bg = colors.bg },
+			c = { fg = colors.yellow, bg = colors.bg },
+			x = { fg = colors.yellow, bg = colors.bg },
+			y = { fg = colors.yellow, bg = colors.bg },
+			z = { fg = colors.yellow, bg = colors.bg },
+		},
+		inactive = {
+			a = { fg = colors.bg_alt, bg = colors.bg },
+			b = { fg = colors.bg_alt, bg = colors.bg },
+			c = { fg = colors.bg_alt, bg = colors.bg },
+			x = { fg = colors.bg_alt, bg = colors.bg },
+			y = { fg = colors.bg_alt, bg = colors.bg },
+			z = { fg = colors.bg_alt, bg = colors.bg },
+		},
+	}
+end
