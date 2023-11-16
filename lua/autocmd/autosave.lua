b1ec23443dfd4b657f340f85990d3a33db678468
tpatalas <tp@atalas.dev>
2023-11-16 15:02:04 -0600

    refactor: Change the debouncing timer to 300ms from 500ms

--- lua/autocmd/autosave.lua
+++ lua/autocmd/autosave.lua
@@ -3,14 +3,14 @@
 ---------------
 local last_time = 0
 
-vim.api.nvim_create_autocmd({ 'TextChanged', 'InsertLeave' }, {
+vim.api.nvim_create_autocmd({ 'TextChanged', 'TextChangedI', 'InsertLeave' }, {
 	pattern = '*.*',
 	callback = function()
 		if vim.bo.filetype ~= 'gitcommit' and vim.fn.expand('%') ~= 'wezterm.lua' then
 			local current_time = vim.fn.reltimefloat(vim.fn.reltime())
-			if current_time - last_time > 0.5 then -- Check if 500ms have passed
+			if current_time - last_time > 0.5 then -- Check if 300ms have passed
 				last_time = current_time
-				vim.cmd('silent! write')
+				vim.cmd('silent! write!')
 			end
 		end
 	end,
