c9384fa5b4b640cbf1e5b0e40a183c44e6e9efb0
tpatalas <tp@atalas.dev>
2023-11-21 17:17:20 -0600

    refactor: Refactor the ts_context_commenstring_module

--- lua/plugins/ts-comment/init.lua
+++ lua/plugins/ts-comment/init.lua
@@ -1,7 +1,8 @@
+vim.g.skip_ts_context_commentstring_module = true
+
 return {
 	'JoosepAlviste/nvim-ts-context-commentstring',
 	config = function()
-		vim.g.skip_ts_context_commentstring_module = true
 		require('ts_context_commentstring').setup({
 			enable_autocmd = false,
 		})
