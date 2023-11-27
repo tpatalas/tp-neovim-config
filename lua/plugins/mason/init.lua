1bcc33a388a823bb224a7559df5b2921061d3e04
tpatalas <tp@atalas.dev>
2023-11-27 11:03:13 -0600

    feat: Remove the unused plugin: mason-null-ls

--- lua/plugins/mason/init.lua
+++ lua/plugins/mason/init.lua
@@ -2,7 +2,6 @@ return {
 	'williamboman/mason.nvim',
 	dependencies = {
 		'williamboman/mason-lspconfig.nvim',
-		'jayp0521/mason-null-ls.nvim',
 	},
 	config = function()
 		local mason = require('mason')
@@ -38,18 +37,6 @@ return {
 			},
 			-- auto-install configured servers (with lspconfig)
 			automatic_installation = true, -- not the same as ensure_installed
-		})
-
-		mason_null_ls.setup({
-			-- list of formatters & linters for mason to install
-			ensure_installed = {
-				'prettier', -- ts/js formatter
-				'stylua', -- lua formatter
-				'eslint_d', -- ts/js linter
-				'markdownlint', -- markdown linter
-			},
-			-- auto-install configured formatters & linters (with null-ls)
-			automatic_installation = true,
 		})
 	end,
 }
