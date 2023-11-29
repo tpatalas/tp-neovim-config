20aaaf58d1da162e0a46e81a21103892bc7a3083
tpatalas <tp@atalas.dev>
2023-11-29 10:52:10 -0600

    feat: Add the new plugin nvim_lint with eslint_d

---
+++ lua/plugins/nvim_lint/init.lua
@@ -0,0 +1,19 @@
+return {
+	'mfussenegger/nvim-lint',
+	config = function()
+		local lint = require('lint')
+
+		lint.linters_by_ft = {
+			javascript = { 'eslint_d' },
+			javascriptreact = { 'eslint_d' },
+			typescript = { 'eslint_d' },
+			typescriptreact = { 'eslint_d' },
+		}
+
+		vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufWritePost', 'InsertLeave' }, {
+			callback = function()
+				require('lint').try_lint()
+			end,
+		})
+	end,
+}
