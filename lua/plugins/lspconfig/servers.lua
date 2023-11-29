0e51dd55fea5afa149cf1306411d647a2fd31057
tpatalas <tp@atalas.dev>
2023-11-29 14:59:16 -0600

    feat: Add the new formatting plugin: conform

--- lua/plugins/lspconfig/servers.lua
+++ lua/plugins/lspconfig/servers.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 local cmp_nvim_lsp = require('cmp_nvim_lsp')
-
+local get_included_filetypes = require('utils.filetype_util')
 local on_attach = function(client, bufnr) end
 local capabilities = cmp_nvim_lsp.default_capabilities()
 local default_configs = {
@@ -17,44 +17,6 @@ M.html = {
 		publish_diagnostic_on = 'insert_leave',
 	},
 }
-
--- local function organize_imports()
--- 	local params = {
--- 		command = '_typescript.organizeImports',
--- 		arguments = { vim.api.nvim_buf_get_name(0) },
--- 		title = '',
--- 	}
--- 	vim.lsp.buf.execute_command(params)
--- end
-
--- M.tsserver = {
--- 	capabilities = capabilities,
--- 	on_attach = on_attach,
--- 	settings = {
--- 		separate_diagnostic_server = true,
--- 		publish_diagnostic_on = 'change',
--- 		tsserver_max_memory = 'auto',
--- 	},
--- commands = {
--- 	OrganizeImports = {
--- 		organize_imports,
--- 		description = 'Organize Imports',
--- 	},
--- },
--- 	handlers = {
--- 		['textDocument/publishDiagnostics'] = function(_, result, ctx, ...)
--- 			local client = vim.lsp.get_client_by_id(ctx.client_id)
---
--- 			if client and client.name == 'tsserver' then
--- 				result.diagnostics = vim.tbl_filter(function(diagnostic)
--- 					return diagnostic.code ~= 80001
--- 				end, result.diagnostics)
--- 			end
---
--- 			return vim.lsp.diagnostic.on_publish_diagnostics(nil, result, ctx, ...)
--- 		end,
--- 	},
--- }
 
 M.dockerls = default_configs
 M.jsonls = default_configs
@@ -133,6 +95,12 @@ M.lua_ls = {
 			},
 		},
 	},
+}
+
+M.typos_lsp = {
+	capabilities = capabilities,
+	on_attach = on_attach,
+	filetypes = get_included_filetypes({ 'toggleterm' }),
 }
 
 return M
