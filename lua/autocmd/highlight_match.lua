068182052f1f86837265c8453fa36859681b3ec7
tpatalas <tp@atalas.dev>
2024-01-05 21:16:21 -0600

    refactor: Enhance auto-highlight efficiency
    Enhance the auto-highlight functionality in autocmd, optimizing the
    highlighting and clearing process of words under the cursor. This update
    introduces an optional delay timer for better performance.

--- lua/autocmd/highlight_match.lua
+++ lua/autocmd/highlight_match.lua
@@ -1,10 +1,26 @@
----------------------------------------------------
--- highlight and autosave the match under cursor --
----------------------------------------------------
-local function highlightCurrentWord(event)
-	local word = event ~= '' and event or vim.fn.expand('<cword>')
+-------------------------------------------------------------
+-- highlight and autosave the match under cursor with delay--
+-------------------------------------------------------------
+local function clearHighlight()
+	vim.cmd('match none')
+end
+
+local function isCursorOnWord()
+	local current_line = vim.api.nvim_get_current_line()
+	local col = vim.api.nvim_win_get_cursor(0)[2] + 1
+	local char = string.sub(current_line, col, col)
+	return char:match('%w') ~= nil
+end
+
+local function highlightCurrentWord()
+	if not isCursorOnWord() then
+		return
+	end
+	local word = vim.fn.expand('<cword>')
 	local escaped_word = vim.fn.escape(word, '/\\')
-	vim.cmd(string.format('match IncSearch /\\V\\<%s\\>/', escaped_word))
+	if escaped_word ~= '' then
+		vim.cmd(string.format('match IncSearch /\\V\\<%s\\>/', escaped_word))
+	end
 end
 
 local group = vim.api.nvim_create_augroup('AutoHighlight', { clear = true })
@@ -24,12 +40,38 @@ local function shouldHighlight()
 	return true
 end
 
-vim.api.nvim_create_autocmd({ 'CursorMovedI', 'CursorMoved' }, {
+local timer = nil
+local delay_ms = 300
+local last_position = nil
+
+local function startHighlightTimer()
+	if timer then
+		vim.fn.timer_stop(timer)
+	end
+	timer = vim.fn.timer_start(delay_ms, function()
+		local current_position = vim.api.nvim_win_get_cursor(0)
+		if current_position[1] == last_position[1] and current_position[2] == last_position[2] then
+			if shouldHighlight() then
+				highlightCurrentWord()
+			end
+		end
+	end)
+end
+
+vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
 	group = group,
 	pattern = '*',
 	callback = function()
-		if shouldHighlight() then
-			highlightCurrentWord('')
-		end
+		clearHighlight()
+		last_position = vim.api.nvim_win_get_cursor(0)
+		startHighlightTimer()
+	end,
+})
+
+vim.api.nvim_create_autocmd('BufLeave', {
+	group = group,
+	pattern = '*',
+	callback = function()
+		clearHighlight()
 	end,
 })
