9e1a6b98557781a48e3e29cc9399157e58abf1b4
tpatalas <tp@atalas.dev>
2024-01-15 13:39:32 -0600

    feat: Remove mason-lspconfig plugin

--- lua/plugins/mason/init.lua
+++ lua/plugins/mason/init.lua
@@ -2,13 +2,8 @@ return {
 	'williamboman/mason.nvim',
 	lazy = true,
 	event = 'VeryLazy',
-	dependencies = {
-		'williamboman/mason-lspconfig.nvim',
-	},
 	config = function()
 		local mason = require('mason')
-		local mason_lspconfig = require('mason-lspconfig')
-		-- enable mason
 		mason.setup({
 			ui = {
 				icons = {
@@ -20,24 +15,6 @@ return {
 				height = 0.8,
 				weight = 0.8,
 			},
-		})
-
-		mason_lspconfig.setup({
-			ensure_installed = {
-				'html',
-				'bashls',
-				'cssls',
-				'dockerls',
-				'eslint',
-				'jsonls',
-				'marksman',
-				'yamlls',
-				'tailwindcss',
-				'lua_ls',
-				'emmet_ls',
-			},
-			-- auto-install configured servers (with lspconfig)
-			automatic_installation = true, -- not the same as ensure_installed
 		})
 	end,
 }
