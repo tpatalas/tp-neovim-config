224eb2938285d64be1249dd9d7e14ca2a1b7122b
tpatalas <tp@atalas.dev>
2024-01-15 08:53:40 -0600

    feat: Add web-devicon as standalong plugin

---
+++ lua/plugins/web-devicon/init.lua
@@ -0,0 +1,17 @@
+-- https://github.com/nvim-tree/nvim-web-devicons
+-- https://www.nerdfonts.com/cheat-sheet
+
+return {
+	'nvim-tree/nvim-web-devicons',
+	lazy = true,
+	ft = { 'norg', 'oil' },
+	opts = {
+		override_by_extension = {
+			['norg'] = {
+				icon = '',
+				color = '#51a0cf',
+				name = 'Neorg',
+			},
+		},
+	},
+}
