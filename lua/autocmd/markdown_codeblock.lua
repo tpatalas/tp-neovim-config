319f88cdcb0c66c8fa0b8d90eef22671cad26d4f
tpatalas <tp@atalas.dev>
2024-01-22 11:03:55 -0600

    refactor: Improve the performance

--- lua/autocmd/markdown_codeblock.lua
+++ lua/autocmd/markdown_codeblock.lua
@@ -1,37 +1,38 @@
-vim.api.nvim_create_autocmd({ 'BufEnter', 'BufRead', 'BufWinEnter', 'TextChanged', 'InsertLeave' }, {
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufRead', 'BufWinEnter', 'TextChanged' }, {
 	pattern = '*.md',
 	callback = function()
-		local bufnr = vim.api.nvim_get_current_buf()
-		local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-		local in_code_block = false
-		local namespace = vim.api.nvim_create_namespace('markdown_code_block')
+		vim.defer_fn(function() -- Defer execution
+			local bufnr = vim.api.nvim_get_current_buf()
+			local start_line = vim.fn.line('w0') -- Start of visible window
+			local end_line = vim.fn.line('w$') -- End of visible window
+			local lines = vim.api.nvim_buf_get_lines(bufnr, start_line - 1, end_line, false)
+			local in_code_block = false
+			local namespace = vim.api.nvim_create_namespace('markdown_code_block')
+			local win_width = vim.api.nvim_win_get_width(0)
 
-		-- Clear existing highlights
-		vim.api.nvim_buf_clear_namespace(bufnr, namespace, 0, -1)
-
-		for i, line in ipairs(lines) do
-			if line:match('^```') then
-				in_code_block = not in_code_block
-				-- Skip highlighting the ``` delimiter lines
-				if in_code_block then
-					goto continue
+			for i, line in ipairs(lines) do
+				if line:match('^```') then
+					in_code_block = not in_code_block
+					if in_code_block then
+						goto continue
+					end
 				end
+
+				if in_code_block then
+					local adjusted_line_num = start_line + i - 2 -- Adjust line number for visible range
+					vim.api.nvim_buf_add_highlight(bufnr, namespace, 'MarkdownCodeBlock', adjusted_line_num, 0, #line)
+					local fill_width = win_width - #line
+					local fill_text = string.rep(' ', fill_width)
+
+					vim.api.nvim_buf_set_extmark(bufnr, namespace, adjusted_line_num, #line, {
+						virt_text = { { fill_text, 'MarkdownCodeBlock' } },
+						virt_text_pos = 'overlay',
+						hl_mode = 'combine',
+					})
+				end
+
+				::continue::
 			end
-
-			if in_code_block then
-				vim.api.nvim_buf_add_highlight(bufnr, namespace, 'MarkdownCodeBlock', i - 1, 0, #line)
-
-				local fill_width = vim.api.nvim_win_get_width(0) - #line
-				local fill_text = string.rep(' ', fill_width)
-
-				vim.api.nvim_buf_set_extmark(bufnr, namespace, i - 1, #line, {
-					virt_text = { { fill_text, 'MarkdownCodeBlock' } },
-					virt_text_pos = 'overlay',
-					hl_mode = 'combine',
-				})
-			end
-
-			::continue::
-		end
+		end, 0)
 	end,
 })
