9ead84d8e7d153bf75608a0350d6e38698bd3238
tpatalas <tp@atalas.dev>
2024-01-23 15:48:18 -0600

    feat: Add the new autocmd markdown-quote

--- lua/autocmd/markdown_conceal.lua
+++ lua/autocmd/markdown_conceal.lua
@@ -3,8 +3,7 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter', 'BufRead', 'BufNewFile'
 	callback = function()
 		-- prevent [[]] to be concealed as quote such as wiki link from Obsidian
 		vim.cmd([[syntax match MarkdownNoConceal '\[\[.\{-}\]\]' containedin=ALL]])
-		-- conceal quote in markdown
-		vim.cmd([[syntax match MarkdownConcealGreater /^>/ contains=MarkdownNoConceal conceal cchar=┃]])
+
 		-- prevent codeblock comment get concealed
 		vim.cmd([[syntax region MarkdownCodeBlock start=/```/ end=/```/ keepend]])
 		-- conceal Headers (nerdfonts are used)
@@ -14,7 +13,25 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter', 'BufRead', 'BufNewFile'
 		vim.cmd([[syntax match MarkdownConcealH4 '^####\ze\s' conceal cchar=󰉮]])
 		vim.cmd([[syntax match MarkdownConcealH5 '^#####\ze\s' conceal cchar=󰉯]])
 		vim.cmd([[syntax match MarkdownConcealH6 '^######\ze\s' conceal cchar=󰉰]])
+		-- conceal strikethrough
+		vim.cmd('highlight MarkdownStrikethrough gui=strikethrough')
+		vim.cmd([[syntax match MarkdownStrikethrough '\v(\s|\_^)\~[^~]+\~(\s|\_$)' contains=MarkdownCodeBlock]])
+		-- conceal italic
+		vim.cmd('highlight MarkdownItalic gui=italic')
+		vim.cmd(
+			[[syntax match MarkdownItalic '\v(\s|\_^)\*[^*]+\*(\s|\_$)' contains=MarkdownStrikethrough,MarkdownCodeBlock]]
+		)
+		-- conceal bold
+		vim.cmd('highlight MarkdownBold gui=bold')
+		vim.cmd(
+			[[syntax match MarkdownBold '\v(\s|\_^)\*\*[^*]+\*\*(\s|\_$)' contains=MarkdownStrikethrough,MarkdownItalic,MarkdownCodeBlock]]
+		)
+		-- conceal bold and italic
+		vim.cmd('highlight MarkdownBoldItalic gui=bolditalic')
+		vim.cmd(
+			[[syntax match MarkdownBoldItalic '\v(\s|\_^)\*\*\*[^*]+\*\*\*(\s|\_$)' contains=MarkdownStrikethrough,MarkdownCodeBlock]]
+		)
 
-		vim.wo.conceallevel = 1
+		vim.wo.conceallevel = 2
 	end,
 })
