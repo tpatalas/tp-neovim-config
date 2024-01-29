2692f30f6fb01468b1ff88275a2d30c7e4b6525a
tpatalas <tp@atalas.dev>
2024-01-28 20:36:53 -0600

    feat: Remove the heading concealments

--- lua/autocmd/markdown_conceal.lua
+++ lua/autocmd/markdown_conceal.lua
@@ -4,15 +4,15 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter', 'BufRead', 'BufNewFile'
 		-- prevent [[]] to be concealed as quote such as wiki link from Obsidian
 		vim.cmd([[syntax match MarkdownNoConceal '\[\[.\{-}\]\]' containedin=ALL]])
 
-		-- prevent codeblock comment get concealed
-		vim.cmd([[syntax region MarkdownCodeBlock start=/```/ end=/```/ keepend]])
-		-- conceal Headers (nerdfonts are used)
-		vim.cmd([[syntax match MarkdownConcealH1 '^#\ze\s' conceal cchar=󰉫]])
-		vim.cmd([[syntax match MarkdownConcealH2 '^##\ze\s' conceal cchar=󰉬]])
-		vim.cmd([[syntax match MarkdownConcealH3 '^###\ze\s' conceal cchar=󰉭]])
-		vim.cmd([[syntax match MarkdownConcealH4 '^####\ze\s' conceal cchar=󰉮]])
-		vim.cmd([[syntax match MarkdownConcealH5 '^#####\ze\s' conceal cchar=󰉯]])
-		vim.cmd([[syntax match MarkdownConcealH6 '^######\ze\s' conceal cchar=󰉰]])
+		-- -- prevent codeblock comment get concealed
+		-- vim.cmd([[syntax region MarkdownCodeBlock start=/```/ end=/```/ keepend]])
+		-- -- conceal Headers (nerdfonts are used)
+		-- vim.cmd([[syntax match MarkdownConcealH1 '^#\ze\s' conceal cchar=󰉫]])
+		-- vim.cmd([[syntax match MarkdownConcealH2 '^##\ze\s' conceal cchar=󰉬]])
+		-- vim.cmd([[syntax match MarkdownConcealH3 '^###\ze\s' conceal cchar=󰉭]])
+		-- vim.cmd([[syntax match MarkdownConcealH4 '^####\ze\s' conceal cchar=󰉮]])
+		-- vim.cmd([[syntax match MarkdownConcealH5 '^#####\ze\s' conceal cchar=󰉯]])
+		-- vim.cmd([[syntax match MarkdownConcealH6 '^######\ze\s' conceal cchar=󰉰]])
 		-- conceal strikethrough
 		vim.cmd('highlight MarkdownStrikethrough gui=strikethrough')
 		vim.cmd([[syntax match MarkdownStrikethrough '\v(\s|\_^)\~\~[^~]+\~\~(\s|\_$)' contains=MarkdownCodeBlock]])
