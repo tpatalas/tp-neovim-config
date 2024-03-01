2f3af77b725cf807574f2573eb9c1fd9c13f415d
tpatalas <tp@atalas.dev>
2024-03-01 09:18:49 -0600

    feat: Add conform as new plugin to replace none-ls

---
+++ lua/plugins/conform/init.lua
@@ -0,0 +1,62 @@
+-- https://github.com/stevearc/conform.nvim
+
+return {
+	'stevearc/conform.nvim',
+	enabled = false,
+	lazy = true,
+	event = { 'VeryLazy' },
+	config = function()
+		require('conform').setup({
+			formatters_by_ft = {
+				['_'] = {
+					'trim_whitespace',
+					'trim_newlines',
+					'squeeze_blanks',
+				},
+				['*'] = {
+					'codespell',
+				},
+				markdown = {
+					'markdownlint',
+				},
+				lua = {
+					'stylua',
+				},
+				toml = {
+					'taplo',
+				},
+				sh = {
+					'shfmt',
+				},
+				javascript = {
+					'eslint_d',
+					'rustywind',
+					{ 'prettierd', 'prettier' },
+				},
+				javascriptreact = {
+					'eslint_d',
+					'rustywind',
+					{ 'prettierd', 'prettier' },
+				},
+				typescript = {
+					'eslint_d',
+					'rustywind',
+					{ 'prettierd', 'prettier' },
+				},
+				typescriptreact = {
+					'eslint_d',
+					'rustywind',
+					{ 'prettierd', 'prettier' },
+				},
+				html = {
+					'rustywind',
+				},
+			},
+			format_on_save = {
+				timeout_ms = 1000,
+				lsp_fallback = true,
+			},
+			notify_on_error = false,
+		})
+	end,
+}
