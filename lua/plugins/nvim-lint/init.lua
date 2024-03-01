74ee1e7aea38ca840031b105a08a899c8b92be6b
tpatalas <tp@atalas.dev>
2024-03-01 09:41:10 -0600

    feat: Add nvim-lint as new plugin

---
+++ lua/plugins/nvim-lint/init.lua
@@ -0,0 +1,56 @@
+-- https://github.com/mfussenegger/nvim-lint
+
+return {
+	'mfussenegger/nvim-lint',
+
+	lazy = true,
+	enabled = false,
+	event = 'VeryLazy',
+	config = function()
+		local lint = require('lint')
+		local linters_all_ft = {
+			{ linter = { 'codespell' }, exclude_ft = {}, exclude_file_name = {} },
+		}
+
+		lint.linters_by_ft = {
+			javascript = {
+				'eslint_d',
+			},
+			yaml = {
+				'actionlint',
+			},
+			yml = {
+				'actionlint',
+			},
+			javascriptreact = {
+				'eslint_d',
+			},
+			typescript = {
+				'eslint_d',
+			},
+			typescriptreact = {
+				'eslint_d',
+			},
+			markdown = {
+				'markdownlint',
+			},
+		}
+
+		vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufWritePost', 'InsertLeave' }, {
+			callback = function()
+				local ft = vim.bo.filetype
+				local filename = vim.fn.expand('%:t')
+
+				require('lint').try_lint()
+				for _, linter_config in ipairs(linters_all_ft) do
+					if
+						not vim.tbl_contains(linter_config.exclude_ft, ft)
+						and not vim.tbl_contains(linter_config.exclude_file_name, filename)
+					then
+						require('lint').try_lint({ linter_config.linter })
+					end
+				end
+			end,
+		})
+	end,
+}
