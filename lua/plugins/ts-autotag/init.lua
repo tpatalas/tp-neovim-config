043d3a4942e03fc4de92b48e08b571507b1e9d07
tpatalas <tp@atalas.dev>
2024-05-20 14:03:52 -0500

    feat: Update ts-autotag with breaking changes

--- lua/plugins/ts-autotag/init.lua
+++ lua/plugins/ts-autotag/init.lua
@@ -20,7 +20,21 @@ return {
 		'handlebars',
 		'hbs',
 	},
+	-- Nvim Treesitter Setup is deprecated, use `require('nvim-ts-autotag').setup()` instead.
+
 	config = function()
-		require('nvim-ts-autotag').setup()
+		require('nvim-ts-autotag').setup({
+			opts = {
+				-- Defaults
+				enable_close = true, -- Auto close tags
+				enable_rename = true, -- Auto rename pairs of tags
+				enable_close_on_slash = false, -- Auto close on trailing </
+			},
+			-- per_filetype = {
+			-- ['html'] = {
+			-- 	enable_close = false,
+			-- },
+			-- },
+		})
 	end,
 }
